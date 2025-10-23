c9ca30c975d8d1667a45a73233614d9c679fdbef
AdhvikV21 <adzydarock@gmail.com>
2025-10-23 23:05:22 +1100

    Add files via upload

--- Random Password Generator.py
+++ Random Password Generator.py
@@ -1,3 +1,20 @@
 import random
-random_number = random.randint(1, 1000000000) 
-print(random_number)
+import string
+
+def generate_strong_password(length=16):
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    digits = string.digits
+    special = string.punctuation
+    all_characters = lowercase + uppercase + digits + special
+    password_list = [
+        random.choice(lowercase),
+        random.choice(uppercase),
+        random.choice(digits),
+        random.choice(special)]
+
+    random.shuffle(password_list)
+    return "".join(password_list)
+
+strong_password = generate_strong_password()
+print(strong_password)
